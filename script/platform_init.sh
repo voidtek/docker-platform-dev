e9b0fbe5db9ba61bde66f3013cd8a1fe4b57eed9
Voidtek <vdacosta@voidtek.com>
2018-04-12 10:08:50 +0000

    Update init sh script.

--- script/platform_init.sh
+++ script/platform_init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd ..
-
 git clone https://github.com/ec-europa/platform-dev.git
 git clone https://github.com/ec-europa/atomium.git
 git clone https://github.com/ec-europa/ec_resp.git
@@ -9,9 +8,11 @@ git clone https://github.com/ec-europa/ec_europa.git
 
 cp build.properties.local platform-dev/
 
+cd script
 ./platform_start.sh
 ./platform_build.sh
 
+cd ..
 rm -rf platform-dev/profiles/multisite_drupal_standard/themes/*
 ln -s ../../../../atomium ./platform-dev/profiles/multisite_drupal_standard/themes/
 ln -s ../../../../ec_resp ./platform-dev/profiles/multisite_drupal_standard/themes/
