4e834ebb4ef77b7eca5fd9653337fe988332cc77
Mabika <francis.mabika1@ext.ec.europa.eu>
2018-08-06 17:57:46 +0000

    Update settings for c9 manage permissions.

--- script/platform_init.sh
+++ script/platform_init.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 cd ..
+echo '============== Clone other repositories ============='
 git clone https://github.com/ec-europa/platform-dev.git
 git clone https://github.com/ec-europa/atomium.git
 git clone https://github.com/ec-europa/ec_resp.git
 git clone https://github.com/ec-europa/ec_europa.git
 
+echo '============== Copy properties file inside the platform ============='
 cp build.properties.local platform-dev/
+
 
 script/platform_start.sh
 script/platform_build.sh
 
-cd ..
+
+
+echo '============== Symlinks Standard profile ============='
 rm -rf platform-dev/profiles/multisite_drupal_standard/themes/*
 ln -s ../../../../atomium ./platform-dev/profiles/multisite_drupal_standard/themes/
 ln -s ../../../../ec_resp ./platform-dev/profiles/multisite_drupal_standard/themes/
 ln -s ../../../../ec_europa ./platform-dev/profiles/multisite_drupal_standard/themes/
 
+echo '============== Symlinks Communities profile ============='
 rm -rf platform-dev/profiles/multisite_drupal_communities/themes/*
 ln -s ../../../../atomium ./platform-dev/profiles/multisite_drupal_communities/themes/
 ln -s ../../../../ec_resp ./platform-dev/profiles/multisite_drupal_communities/themes/
